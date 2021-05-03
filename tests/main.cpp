73556b6f0d94868f7b0d847511d74c6e514354b2
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-05-03 12:47:52 +0300

    make tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,3 +1,20 @@
+#include <iostream>
+#include <cpp-unit-test.h>
+
+CppUnitTest::TestCase* testCase01()
+{
+    CppUnitTest::TestCase* t = new CppUnitTest::TestCase("001-simple-test");
+    t->printTitle();
+    t->finish();
+    return t;
+}
+
 int main() {
+    CppUnitTest::TestSuite testSuite;
+
+    testSuite.addTestCase(testCase01());
+
+    testSuite.printTotal();
+
     return 0;
 }
