37edb24bae936bcd2ce237d7627d337b132103e6
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-05-03 13:06:29 +0300

    parse tokens

--- include/regexp/lexer.h
+++ include/regexp/lexer.h
@@ -14,6 +14,8 @@ namespace RegExp
 
     private:
         std::string* expr;
+
+        Token* getNextToken();
     };
 }
 
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -9,6 +9,19 @@ namespace RegExp
 
     std::list<Token*>* Lexer::parseTokens()
     {
+        auto tokens = new std::list<Token*>;
+        while (true) {
+            auto token = this->getNextToken();
+            if (token == nullptr) {
+                break;
+            }
+            tokens->push_back(token);
+        }
+        return tokens;
+    }
+
+    Token* Lexer::getNextToken()
+    {
         return nullptr;
     }
 }
