85759f5315f6cb72191908a5351dce8b108fff3a
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-05-07 22:19:50 +0300

    finish token map

--- src/token/token_map.cpp
+++ src/token/token_map.cpp
@@ -7,26 +7,24 @@ namespace RegExp::Token
     TokenMap::TokenMap()
     {
         this->tokenMap = new std::map<std::string, int>{
-                {"quantifier",          TokenType::Quantifier},
-                {"captureOpen",         TokenType::CaptureOpen},
-                {"captureClose",        TokenType::CaptureClose},
-                {"squareBracketOpen",   TokenType::SquareBracketOpen},
-                {"squareBracketClose",  TokenType::SquareBracketClose},
-                {"match",               TokenType::Match},
-                {"quantifierNumber",    TokenType::QuantifierNumber},
-                {"quantifierDelimiter", TokenType::QuantifierDelimiter},
-                {"quantifierOpen",      TokenType::QuantifierOpen},
-                {"quantifierClose",     TokenType::QuantifierClose},
-                {"alphabet",            TokenType::Alphabet},
-                {"alphabetRange",       TokenType::AlphabetRange},
-                {"invertAlphabet",      TokenType::InvertAlphabet},
-                {"oneof",               TokenType::OneOfVariant},
-                /**
-        StartOfLine, // ^
-        EndOfLine, // $
-        AnySymbol, // .
-        Whitespace, // \s
-                 */
+            {"quantifier",          TokenType::Quantifier},
+            {"captureOpen",         TokenType::CaptureOpen},
+            {"captureClose",        TokenType::CaptureClose},
+            {"squareBracketOpen",   TokenType::SquareBracketOpen},
+            {"squareBracketClose",  TokenType::SquareBracketClose},
+            {"match",               TokenType::Match},
+            {"quantifierNumber",    TokenType::QuantifierNumber},
+            {"quantifierDelimiter", TokenType::QuantifierDelimiter},
+            {"quantifierOpen",      TokenType::QuantifierOpen},
+            {"quantifierClose",     TokenType::QuantifierClose},
+            {"alphabet",            TokenType::Alphabet},
+            {"alphabetRange",       TokenType::AlphabetRange},
+            {"invertAlphabet",      TokenType::InvertAlphabet},
+            {"oneof",               TokenType::OneOfVariant},
+            {"startOfLine",         TokenType::StartOfLine},
+            {"endOfLine",           TokenType::EndOfLine},
+            {"anySymbol",           TokenType::AnySymbol},
+            {"whitespace",          TokenType::Whitespace},
         };
     }
 }
