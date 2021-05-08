af6fa7c18f66b8945cd568d225b58514d0469109
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-05-08 12:48:57 +0300

    update

--- tests/src/test-cases/predicate_tests.cpp
+++ tests/src/test-cases/predicate_tests.cpp
@@ -24,6 +24,35 @@ namespace RegExpTests
         CppUnitTest::assertTrue(t, p->calc("alexander"));
         CppUnitTest::assertTrue(t, p->calc("alexander_1990"));
         CppUnitTest::assertTrue(t, p->calc("test_case#@123!"));
+        CppUnitTest::assertFalse(t, p->calc("test_case#@123!-"));
+
+        t->finish();
+        return t;
+    }
+
+    CppUnitTest::TestCase* testPredicate_InvertAlphabet_TestCase01()
+    {
+        auto t = new CppUnitTest::TestCase("testPredicateTestCase01: [!@#a-z*_0-9]");
+        t->printTitle();
+
+        RegExp::Lexer lexer("[!@#a-z*_0-9]");
+        auto tokens = lexer.parseTokens();
+        auto tokensIt = tokens->begin();
+
+        tokensIt++; // skip [
+        auto testTokens = new std::list<SyntaxTree::Token::Token*>;
+
+        do {
+            testTokens->push_back(*tokensIt);
+            tokensIt++;
+        } while (std::next(tokensIt) != tokens->end());
+
+        auto pa = new RegExp::Predicate::Alphabet(testTokens);
+        auto p = new RegExp::Predicate::Invert(pa);
+        CppUnitTest::assertFalse(t, p->calc("alexander"));
+        CppUnitTest::assertFalse(t, p->calc("alexander_1990"));
+        CppUnitTest::assertFalse(t, p->calc("test_case#@123!"));
+        CppUnitTest::assertTrue(t, p->calc("test_case#@123!-"));
 
         t->finish();
         return t;
