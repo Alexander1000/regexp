320da7d7256102d35b8888d339a727aee62bb4aa
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-05-15 13:32:20 +0300

    make scaner

--- src/scaner/scaner.cpp
+++ src/scaner/scaner.cpp
@@ -1,3 +1,4 @@
+#include <memory>
 #include <regexp.h>
 
 namespace RegExp::Scaner
@@ -5,5 +6,41 @@ namespace RegExp::Scaner
     Scaner::Scaner(std::list<RegExp::Predicate::Predicate*>* predicates)
     {
         this->predicates = predicates;
+    }
+
+    char* Scaner::scan(const char *input)
+    {
+        auto chars = new char[2];
+        memset(chars, 0, sizeof(char) * 2);
+        int nCurChar = 0;
+
+        for (int i = 0; i < strlen(input); i++) {
+            chars[0] = input[i];
+
+            for (auto & predicate : *this->predicates) {
+                if (predicate->calc(chars)) {
+                    nCurChar++;
+                    break;
+                }
+            }
+
+            if (nCurChar != i + 1) {
+                break;
+            }
+        }
+
+        if (nCurChar > 0) {
+            auto scannedString = (char*) malloc(sizeof(char) * (nCurChar + 1));
+            memset(scannedString, 0, sizeof(char) * (nCurChar + 1));
+            memcpy(scannedString, input, sizeof(char) * nCurChar);
+            return scannedString;
+        }
+
+        return nullptr;
+    }
+
+    char * Scaner::scan(std::string *input)
+    {
+        return nullptr;
     }
 }
